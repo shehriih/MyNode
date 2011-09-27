188a062c47d133e7fadf96c4d6034f679d6d17ba
Ibrahim ALShehri <ibrahim.h@gmail.com>
2011-09-27 21:57:51 +0300

    fix

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var http = require('http')
 var server = http.creatServer(function (req,res)
 {
 	res.writeHead(200,{"Content-Type": "text/plain"})
-	res.end("Hello World\n);
+	res.end("Hello World\n");
 });
 
 server.listen(process.env.PORT || 8001);
